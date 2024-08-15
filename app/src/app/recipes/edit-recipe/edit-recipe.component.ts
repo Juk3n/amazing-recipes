98d828ba12f4e0d45fb65fc7fb35a9669f8550eb
juk3n <adekenn3@gmail.com>
2024-08-15 16:12:51 +0200

    Added edit recipe form

--- app/src/app/recipes/edit-recipe/edit-recipe.component.ts
+++ app/src/app/recipes/edit-recipe/edit-recipe.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Recipe } from '../recipe/recipe.model';
 
 @Component({
   selector: 'app-edit-recipe',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './edit-recipe.component.html',
   styleUrl: './edit-recipe.component.css'
 })
 export class EditRecipeComponent {
+  editedRecipe = input.required<Recipe>();
+  enteredName = signal<string>('');
+  enteredIngedients = signal<string[]>(['']);
+  enteredSteps = signal<string[]>(['']);
 
+  ngOnInit() {
+    this.enteredName.set(this.editedRecipe().name);
+    this.enteredIngedients.set(this.editedRecipe().ingredients);
+    this.enteredSteps.set(this.editedRecipe().steps);
+  }
+
+  onSubmit() {
+
+  }
+
+  onAddIngredient() {
+    this.enteredIngedients().push('');
+  }
+
+  onAddStep() {
+    this.enteredSteps().push('');
+  }
 }
